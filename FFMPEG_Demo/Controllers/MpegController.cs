d6e0fe7a192b5218f38ec0c6997a03c06b32ddc3
Samrat <samratdot@outlook.com>
2021-03-28 04:03:38 +0530

    app config constant setting

--- FFMPEG_Demo/Controllers/MpegController.cs
+++ FFMPEG_Demo/Controllers/MpegController.cs
@@ -1,10 +1,12 @@
 ﻿using FFMPEG_Demo.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Web.Http;
 using System.Web.Http.Description;
 
@@ -35,7 +37,8 @@ namespace FFMPEG_Demo.Controllers
         public HttpResponseMessage CreateFolder(string name = null)
         {
             string _alert = null;
-            string path = @"D:\ffmpeg_demo\" + name;
+            string base_content_storage = ConfigurationManager.AppSettings["base_content_storage"];
+            string path = base_content_storage + name;
             if (!(Directory.Exists(path)))
             {
                 Directory.CreateDirectory(path);
@@ -52,7 +55,8 @@ namespace FFMPEG_Demo.Controllers
         public HttpResponseMessage DeleteFolder(string name = null)
         {
             string _alert = null;
-            string path = @"D:\ffmpeg_demo\" + name;
+            string base_content_storage = ConfigurationManager.AppSettings["base_content_storage"];
+            string path = base_content_storage + name;
             if ((Directory.Exists(path)))
             {
                 DeleteDirectory(path);
@@ -71,7 +75,8 @@ namespace FFMPEG_Demo.Controllers
             string _alert = null;
             if (!String.IsNullOrWhiteSpace(createKeyBody.Id))
             {
-                string path = @"D:\ffmpeg_demo\" + createKeyBody.Id;
+                string base_content_storage = ConfigurationManager.AppSettings["base_content_storage"];
+                string path = base_content_storage + createKeyBody.Id;
                 if (!(Directory.Exists(path)))
                 {
                     CreateFolder(createKeyBody.Id);
@@ -79,7 +84,6 @@ namespace FFMPEG_Demo.Controllers
                 //open ssl cmd here
                 RunSSL(createKeyBody.Id);
                 _alert = "Key File Created Successfully";
-
             }
             else
             {
@@ -89,19 +93,17 @@ namespace FFMPEG_Demo.Controllers
             var obj = new { alert = _alert };
             return Request.CreateResponse(HttpStatusCode.OK, obj);
         }
-
         [HttpGet]
         public HttpResponseMessage Keyinfo(string id = null)
         {
-            var path = System.Web.HttpContext.Current.Server.MapPath("~/player_content/" + id);
-            var fullpath = Path.Combine(path, "enc.key");
+            //var path = System.Web.HttpContext.Current.Server.MapPath("~/player_content/" + id);
+            string base_content_storage = ConfigurationManager.AppSettings["base_content_storage"];
+            var fullpath = Path.Combine(base_content_storage, id, "enc.key");
             string _alert = null;
-            if (Directory.Exists(path))
+            if (Directory.Exists(base_content_storage + id))
             {
                 if (File.Exists(fullpath))
                 {
-                    //string data = System.IO.File.ReadAllText(fullpath);
-                    //return Request.CreateResponse(HttpStatusCode.OK, data);
                     var dataBytes = File.ReadAllBytes(fullpath);
                     var dataStream = new MemoryStream(dataBytes);
                     HttpResponseMessage httpResponseMessage = Request.CreateResponse(HttpStatusCode.OK);
@@ -123,15 +125,49 @@ namespace FFMPEG_Demo.Controllers
             var obj = new { alert = _alert };
             return Request.CreateResponse(HttpStatusCode.OK, obj);
         }
-
         [HttpGet]
         public HttpResponseMessage M3u8info(string id = null)
         {
-            var path = System.Web.HttpContext.Current.Server.MapPath("~/player_content/" + id);
+            //var path = System.Web.HttpContext.Current.Server.MapPath("~/player_content/" + id);
+            string base_content_storage = ConfigurationManager.AppSettings["base_content_storage"];
             string filename = "index.m3u8";
-            var fullpath = Path.Combine(path, filename);
+            var fullpath = Path.Combine(base_content_storage, id, filename);
             string _alert = null;
-            if (Directory.Exists(path))
+            if (Directory.Exists(base_content_storage + id))
+            {
+                if (File.Exists(fullpath))
+                {
+                    var dataBytes = File.ReadAllBytes(fullpath);
+                    var dataStream = new MemoryStream(dataBytes);
+                    dataStream = M3u8DataManipulation(dataStream, id);
+                    HttpResponseMessage httpResponseMessage = Request.CreateResponse(HttpStatusCode.OK);
+                    httpResponseMessage.Content = new StreamContent(dataStream);
+                    httpResponseMessage.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+                    httpResponseMessage.Content.Headers.ContentDisposition.FileName = filename;
+                    httpResponseMessage.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+                    return httpResponseMessage;
+                }
+                else
+                {
+                    _alert = "file not found";
+                }
+            }
+            else
+            {
+                _alert = "file not found";
+            }
+            var obj = new { alert = _alert };
+            return Request.CreateResponse(HttpStatusCode.OK, obj);
+        }
+        [HttpGet]
+        public HttpResponseMessage Tsinfo(string id = null, string filename = null)
+        {
+            //var path = System.Web.HttpContext.Current.Server.MapPath("~/player_content/" + id);
+            string base_content_storage = ConfigurationManager.AppSettings["base_content_storage"];
+            //string filename = "index.m3u8";
+            var fullpath = Path.Combine(base_content_storage, id, filename + ".ts");
+            string _alert = null;
+            if (Directory.Exists(base_content_storage + id))
             {
                 if (File.Exists(fullpath))
                 {
@@ -140,12 +176,9 @@ namespace FFMPEG_Demo.Controllers
                     HttpResponseMessage httpResponseMessage = Request.CreateResponse(HttpStatusCode.OK);
                     httpResponseMessage.Content = new StreamContent(dataStream);
                     httpResponseMessage.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
-                    httpResponseMessage.Content.Headers.ContentDisposition.FileName = filename;
+                    httpResponseMessage.Content.Headers.ContentDisposition.FileName = filename + ".ts";
                     httpResponseMessage.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
                     return httpResponseMessage;
-
-                    //string data = System.IO.File.ReadAllText(fullpath);
-                    //return Request.CreateResponse(HttpStatusCode.OK, data);
                 }
                 else
                 {
@@ -161,11 +194,12 @@ namespace FFMPEG_Demo.Controllers
         }
 
         [HttpGet]
-        public HttpResponseMessage Tsinfo(string id = null, string filename = null)
+        public HttpResponseMessage M3u8infoNew(string id = null)
         {
-            var path = System.Web.HttpContext.Current.Server.MapPath("~/player_content/" + id);
-            //string filename = "index.m3u8";
-            var fullpath = Path.Combine(path, filename + ".ts");
+            string path = @"D:\ffmpeg_demo\" + id;
+            //var path = System.Web.HttpContext.Current.Server.MapPath("~/player_content/" + id);
+            string filename = "index.m3u8";
+            var fullpath = Path.Combine(path, filename);
             string _alert = null;
             if (Directory.Exists(path))
             {
@@ -173,15 +207,14 @@ namespace FFMPEG_Demo.Controllers
                 {
                     var dataBytes = File.ReadAllBytes(fullpath);
                     var dataStream = new MemoryStream(dataBytes);
+                    dataStream = M3u8DataManipulation(dataStream, id);
+
                     HttpResponseMessage httpResponseMessage = Request.CreateResponse(HttpStatusCode.OK);
                     httpResponseMessage.Content = new StreamContent(dataStream);
                     httpResponseMessage.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
-                    httpResponseMessage.Content.Headers.ContentDisposition.FileName = filename + ".ts";
+                    httpResponseMessage.Content.Headers.ContentDisposition.FileName = filename;
                     httpResponseMessage.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
                     return httpResponseMessage;
-
-                    //string data = System.IO.File.ReadAllText(fullpath);
-                    //return Request.CreateResponse(HttpStatusCode.OK, data);
                 }
                 else
                 {
@@ -255,17 +288,38 @@ namespace FFMPEG_Demo.Controllers
             }
         }
 
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [NonAction]
+        private MemoryStream M3u8DataManipulation(MemoryStream _stream, string id)
+        {
+            StreamReader dataReader = new StreamReader(_stream);
+            string dataText = dataReader.ReadToEnd();
+            // data Manipulation
+            //string id = "20210327-181439-b9093c54-0c6e-4ae6-bdac-c70f0e9f7a95";
+            //string base_api_Keyinfo = @"http://localhost:50017/api/mpeg/Keyinfo/";
+            //string base_api_Tsinfo = @"http://localhost:50017/api/mpeg/Tsinfo/";
 
+            string base_api_Keyinfo = ConfigurationManager.AppSettings["base_api_Keyinfo"];
+            string base_api_Tsinfo = ConfigurationManager.AppSettings["base_api_Tsinfo"];
 
+            string pattern_URI = "URI=\"(.)+\"";  // http://localhost:50017/api/mpeg/Keyinfo/20210327-181439-b9093c54-0c6e-4ae6-bdac-c70f0e9f7a95
+            string newURI = "URI=\"" + base_api_Keyinfo + id + "\"";
 
+            string pattern_segment = "(segmentNo)";
+            string newSegment = base_api_Tsinfo + id + "/segmentNo";  // http://localhost:50017/api/mpeg/Tsinfo/{id}/segmentNo0
 
+            //RegexOptions options = RegexOptions.IgnoreCase | RegexOptions.Multiline;
+            dataText = Regex.Replace(dataText, pattern_URI, newURI);
+            dataText = Regex.Replace(dataText, pattern_segment, newSegment);
+            dataText = Regex.Replace(dataText, "(.ts)", "");
 
-
-
-
-
-
-
+            var manipulationStream = new MemoryStream();
+            var writer = new StreamWriter(manipulationStream);
+            writer.Write(dataText);
+            writer.Flush();
+            manipulationStream.Position = 0;
+            return manipulationStream;
+        }
 
 
         #endregion
