79d6a7970dd7d93acdeba44d7544493257dbd063
Samrat <samratdot@outlook.com>
2021-04-01 17:16:19 +0530

    .

---
+++ FFMPEG_Demo/note/SQL_Insert_Chapter.sql
@@ -0,0 +1,22 @@
+﻿USE [FFMpeg]
+GO
+
+INSERT INTO [dbo].[tblChapter]
+           ([chapterName]
+           ,[subjectId]
+           ,[classId])
+     VALUES
+           (N'পরিবেশ ও উৎপাদন (কৃষি ও মৎস্য উৎপাদন)'
+           ,5
+           ,1)
+GO
+
+SELECT * FROM [dbo].[tblChapter] order by id desc
+
+--SELECT * FROM [dbo].[tblClass]
+--SELECT * FROM [dbo].[tblSubject]
+--SELECT * FROM [dbo].[tblChapter]
+
+
+
+
